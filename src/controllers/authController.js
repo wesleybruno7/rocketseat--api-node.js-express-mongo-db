a676c0dfa14590a7b45a2484e0e9511662337bd0
wesleybruno7 <wesleybruno7@terra.com.br>
2020-03-10 13:42:57 -0300

    Atualizando projeto (adicionando e validando token jwt)
    Concluido segundo video:
    https://www.youtube.com/watch?v=KKTX1l3sZGk&list=PL85ITvJ7FLoiXVwHXeOsOuVppGbBzo2dp&index=2

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const authConfig = require('../config/auth')
 
 const User = require('../models/User')
 
 const router = express.Router()
+
+function generateToken(params = {  }) {
+
+    // o valor "expiresIn: 86400" corresponde a 1 dia para expirar o token
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400
+    })
+
+}
 
 router.post('/register', async (req, res) => {
     
@@ -14,10 +27,12 @@ router.post('/register', async (req, res) => {
         
         const user = await User.create(req.body)
 
-        user.passVisible = undefined
         user.password = undefined
 
-        return res.send({ user })
+        return res.send({ 
+            user,
+            token: generateToken({ id: user.id })
+        })
 
     } catch (err) {
 
@@ -26,6 +41,26 @@ router.post('/register', async (req, res) => {
 
     }
 
+})
+
+router.post('/authenticate', async (req, res) => {
+
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email }).select('+password')
+
+    if (!user)
+        return res.status(400).send({ error: 'User not found' })
+
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: 'Invalid password' })
+
+    user.password = undefined // oculta a senha do retorno da api
+
+    res.send({ 
+        user, 
+        token: generateToken({ id: user.id })
+    })
 })
 
 //recebe o app do src/index.js e retorna o app com o prefixo /auth.
