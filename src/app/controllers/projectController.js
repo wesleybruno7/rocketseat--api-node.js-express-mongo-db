f39fb3689d29f009fb09a430bcbbd2e28269cc67
wesleybruno7 <wesleybruno7@terra.com.br>
2020-03-12 15:12:52 -0300

    Faltando pouco para finalizar
    Video concluido:
    https://www.youtube.com/watch?v=GAZdUyIV3ms&list=PL85ITvJ7FLoiXVwHXeOsOuVppGbBzo2dp&index=4
    
    Obs.: ficou um problema com a rota de put (alterar), quando alteramos um projeto, as tasks sao perdidas (esta com erro e ainda nao consegui resolver), os demais metodos/rotas estao ok (get, post e delete)

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -12,7 +12,9 @@ router.use(authMiddleware)
 router.get('/', async (req, res) => {
     try {
 
-        const projects = await Project.find()
+        // o .populate('user') ja executa um filtro trazendo todos os dados 
+        //referentes ao usuario e o adiciona ao json da listagem de projetos
+        const projects = await Project.find().populate(['user', 'tasks'])
 
         return res.send({ projects })
 
@@ -25,20 +27,43 @@ router.get('/', async (req, res) => {
 
 // rota para listar um projeto em especifico
 router.get('/:projectId', async (req, res) => {
-    res.send({ user: req.userId })
-})
-
-// rota para criar o projeto
-router.post('/', async (req, res) => {
     try {
-        
-        // const project = await Project.create(req.body)
-        const project = await Project.create({ ...req.body, user: req.userId } )
+
+        const project = await Project.findById(req.params.projectId).populate(['user', 'tasks'])
 
         return res.send({ project })
 
     } catch (err) {
         
+        return res.status(400).send({ error: 'Error loading project' })
+
+    }
+})
+
+// rota para criar o projeto
+router.post('/', async (req, res) => {
+    try {
+
+        const { title, description, tasks } = req.body
+        // const project = await Project.create(req.body)
+        // const project = await Project.create({ ...req.body, user: req.userId } )
+        const project = await Project.create({ title, description , user: req.userId } )
+
+        await Promise.all(tasks.map(async task => {
+
+            const projectTask = new Task({ ...task, project: project._id })
+
+            await projectTask.save()
+            project.tasks.push(projectTask)
+
+        }))
+
+        await project.save()
+
+        return res.send({ project })
+
+    } catch (err) {
+        console.log(err)
         return res.status(400).send({ error: 'Error creating new project' })
 
     }
@@ -46,12 +71,53 @@ router.post('/', async (req, res) => {
 
 // rota para alterar um projeto especifico
 router.put('/:projectId', async (req, res) => {
-    res.send({ user: req.userId })
+
+    try {
+
+        const { title, description, tasks } = req.body
+
+        const project = await Project.findByIdAndUpdate(req.params.projectId, { 
+            title, 
+            description
+        }, { new: true })
+
+        project.tasks = []
+        await Task.remove({ project: project._id })
+
+        await Promise.all(tasks.map(async task => {
+
+            const projectTask = new Task({ ...task, project: project._id })
+
+            await projectTask.save()
+            project.tasks.push(projectTask)
+
+        }))
+
+        await project.save()
+
+        return res.send({ project })
+
+    } catch (err) {
+        console.log(err)
+        return res.status(400).send({ error: 'Error updating project' })
+
+    }
+    
 })
 
 // rota para deletar um projeto especifico
 router.delete('/:projectId', async (req, res) => {
-    res.send({ user: req.userId })
+    try {
+
+        await Project.findByIdAndRemove(req.params.projectId)
+
+        return res.send()
+
+    } catch (err) {
+        
+        return res.status(400).send({ error: 'Error deleteing project' })
+
+    }
 })
 
 module.exports = app => app.use('/projects', router)
